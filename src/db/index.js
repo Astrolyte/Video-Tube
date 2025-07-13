0288b3ae03b6e181770c0456fd3372579128be78
Aditya Kumar Singh <singhaditya6678@gmail.com>
2025-07-13 19:45:27 +0530

    feat: Enhance user controller and routes with additional functionalities for password change, user retrieval, and watch history

--- src/db/index.js
+++ src/db/index.js
@@ -6,10 +6,9 @@ const connectDB = async()=>{
     try {
       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MONGODB connected: DB HOST: ${connectionInstance.connection.host}`);
-         {}
     } catch (error) {
         console.log("MONGODB conneciton error: ",error);
-        process
+        process.exit(1);
     }
 }
 
